f0120788d16c68de36c39d3e7a5e8cc2e72c1373
Nicole Bartholomew <nabartholomew@gmail.com>
2021-01-07 14:45:14 -0600

    (feat) Added base scss file. Layout and styles for nav.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss', './_base.scss']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'weather-app';
