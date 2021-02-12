a546dcde665112c6f15ae75317360ad5cbe6c567
Nicole Bartholomew <nabartholomew@gmail.com>
2021-02-12 16:28:09 -0600

    (feat) Added google places autocomplete and is working.

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Options } from 'ngx-google-places-autocomplete/objects/options/options';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,13 +7,19 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent implements OnInit {
-  
   formattedAddress: string = "";
   isSearchActive: boolean = false;
-  options = {
-    componentRestricitons: {
-      
-    }
+  options: Options = {
+    fields: ["formatted_address", "address_component", "place_id", "geometry.location"],
+    // fields: [],
+    types: ['(cities)'],
+    // types: ['(postal_code_prefix)'],
+    // types: ['(regions)'],
+    // types: [],
+    bounds: undefined,
+    componentRestrictions: undefined,
+    strictBounds: undefined,
+    origin: undefined
   }
   constructor() { }
 
@@ -25,6 +32,9 @@ export class SearchBarComponent implements OnInit {
 
   addressChange(address: any) {
     this.formattedAddress = address.formatted_address;
+    // this.formattedAddress = address.address_components[address.address_components.length - 1].short_name;
+    console.log(address);
+    console.log(address.geometry.location.lat());
   }
 
 }
