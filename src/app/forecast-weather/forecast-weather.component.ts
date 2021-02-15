589a50fd29fca7861151d61a8cf5b70dab155993
Nicole Bartholomew <nabartholomew@gmail.com>
2021-02-15 16:14:03 -0600

    (feat) Fixed name styling added name loading from observeable.

--- src/app/forecast-weather/forecast-weather.component.ts
+++ src/app/forecast-weather/forecast-weather.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LocationData } from '../service/weather-interface';
+import { WeatherService } from '../service/weather.service';
 
 @Component({
   selector: 'app-forecast-weather',
@@ -7,11 +8,15 @@ import { LocationData } from '../service/weather-interface';
   styleUrls: ['./forecast-weather.component.scss']
 })
 export class ForecastWeatherComponent implements OnInit {
-  locationData: LocationData;
-  constructor() { }
+  locationInfo: LocationData;
+
+  constructor(private weatherService: WeatherService) { }
 
   ngOnInit() {
-    // this.locationData = this
+    this.weatherService .currentLocation
+      .subscribe((currentLocation: LocationData) => {
+        this.locationInfo = currentLocation;
+    });
   }
 
 }
